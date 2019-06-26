672319978810b50847966ebf3ac982f48f2ac379
Julien Sagot <julien.sagot@0x4a.fr>
2019-06-26 20:02:27 +0200

    Added basic benchmark.

---
+++ test/bench.ml
@@ -0,0 +1,6 @@
+let bench name n fn =
+  ignore @@ Benchmark.latency1 ~name n (List.iter fn) Test_utils.data
+
+let () =
+  bench "DoubleMetaphone.double_metaphone" 1000L
+    (fun x -> ignore @@ Phonetic.DoubleMetaphone.double_metaphone x.input)
